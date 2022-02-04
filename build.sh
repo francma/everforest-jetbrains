1950bc6eff05b03278b59e86883625434fa04af4
Martin Franc <me@martinfranc.eu>
2022-02-04 23:41:29 +0100

    Add soft & hard light themes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p theme
 
-for theme in dark_hard dark_medium dark_soft light_medium; do
+for theme in dark_hard dark_medium dark_soft light_hard light_medium light_soft; do
   export $(cat "src/defines.env" "src/pallete_${theme}.env" | xargs)
   cat src/theme_header.xml src/includes/* src/theme_footer.xml | envsubst > theme/everforest_${theme}.xml
   cat src/everforest.theme.json | envsubst > theme/everforest_${theme}.theme.json
